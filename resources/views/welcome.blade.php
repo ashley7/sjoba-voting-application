2b3d77d78fce73525c7101e10807812a55d7e2b6
Ashley7 <ashley7520charles@gmail.com>
2021-07-18 08:55:19 +0300

    Clean ups

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
+        <link rel="shortcut icon" href="{{ asset('/images/logo.jpeg') }}">
+
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <!-- Styles -->
