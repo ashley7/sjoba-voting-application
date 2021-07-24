48c0f182be8351911b9755e35d84b062075a9570
Ashley7 <ashley7520charles@gmail.com>
2021-07-24 16:44:20 +0300

    Clean ups

--- resources/views/voter/list.blade.php
+++ resources/views/voter/list.blade.php
@@ -15,6 +15,7 @@
                             <th>NAME</th>
                             <th>PHONE NUMBER</th>
                             @if(Auth::user()->user_type == "admin")
+                            <th>PIN</th>
                               
                                 <th>ACTION</th> 
                             @endif                            
@@ -26,6 +27,7 @@
                                   <td>{{ $voter->name }}</td>
                                   <td>{{ $voter->phone_number }}</td>
                                   @if(Auth::user()->user_type == "admin")
+                                  <td>{{  $voter->pin  }}</td>
                                                                     
                                     <td>
                                         <form method="POST" action="{{ route('voters.destroy',$voter->id) }}">
