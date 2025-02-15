3ad6a8dcd73bab4d0a1edb87acb322b28badc0da
Ashley7 <ashley7520charles@gmail.com>
2025-02-15 13:49:24 +0300

    Clean ups

--- resources/views/not_yet_time.blade.php
+++ resources/views/not_yet_time.blade.php
@@ -5,8 +5,11 @@
     <h3>{{ $title }}</h3>
     <p>Voting start time: {{ empty($voteTime) ? "Not set": Carbon\Carbon::parse($voteTime->starting_time)->format('Y-m-d h:i a') }}</p>
     <p>Voting end time: {{ empty($voteTime) ? "Not set": Carbon\Carbon::parse($voteTime->end_time)->format('Y-m-d h:i a') }}</p>
+
+    <hr>
+    Current time: {{ now() }}
 </div>
 
-Current time: {{ now() }}
+
 @endsection
  
